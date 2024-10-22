d858efec5db45c546c061308723d5557292302a9
alief abdillah <aliefmabdillah09@gmail.com>
2024-10-22 19:00:43 +0700

    feat: websocket for polling country

--- src/module/country/country.module.ts
+++ src/module/country/country.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { CountryController } from './country.controller';
 import { CountryService } from './country.service';
 import { PrismaService } from 'prisma/prisma.service';
+import { CountryGateway } from './gateway/poll-country.gateway';
 
 @Module({
   controllers: [CountryController],
-  providers: [CountryService, PrismaService],
+  providers: [CountryService, PrismaService, CountryGateway],
 })
 export class CountryModule {}
---
+++ src/module/country/gateway/poll-country.gateway.ts
@@ -0,0 +1,40 @@
+import { Logger } from '@nestjs/common';
+import {
+  MessageBody,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
+import { CountryService } from '../country.service';
+
+@WebSocketGateway(3001, { cors: true })
+export class CountryGateway
+  implements OnGatewayConnection, OnGatewayDisconnect
+{
+  private readonly logger = new Logger();
+  @WebSocketServer() server: Server;
+
+  constructor(private readonly countryService: CountryService) {}
+
+  handleConnection(client: Socket) {
+    const connectedClients = this.server.engine.clientsCount;
+    this.logger.log(`Number of User Connected ${connectedClients}`);
+    this.logger.log(`User Connected with ID: ${client.id}`);
+  }
+
+  handleDisconnect(client: Socket) {
+    this.logger.log(`User Disconnected ${client.id}`);
+    const connectedClients = this.server.engine.clientsCount;
+    this.logger.log(`Number of User Connected ${connectedClients}`);
+  }
+
+  @SubscribeMessage('vote')
+  async handleVoteEvent(@MessageBody('id') id: string) {
+    const countryData = await this.countryService.update(id);
+    console.log(countryData);
+    this.server.emit('country', countryData);
+  }
+}
